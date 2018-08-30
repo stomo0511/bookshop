f5cb6144056440140231722fd04bdd1753135fe0
Tomohiro Suzuki <stomo@yamanashi.ac.jp>
2018-08-30 19:00:16 +0900

    showp

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,26 @@
-from django.contrib import admin
-
 # Register your models here.
+# 20180830 18:53
+
+from django.contrib import admin
+from django import forms
+
+from .models import Author, Book, Publisher
+
+
+class BookAdminForm(forms.ModelForm):
+    def clean_title(self):
+        value = self.cleaned_data['title']
+        if 'Django' not in value:
+            raise forms.ValidationError("タイトルには「Django」という文字を含めてください")
+        return value
+
+
+@admin.register(Book)
+class BookModelAdmin(admin.ModelAdmin):
+    list_display = ('title', 'publisher', 'price')
+    ordering = ('-price',)
+    form = BookAdminForm
+
+
+admin.site.register(Publisher)
+admin.site.register(Author)
---
+++ shop/urls.py
@@ -0,0 +1,12 @@
+# 20180830 18:58
+
+from django.urls import path
+
+from . import views
+
+app_name = 'shop'
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('<int:book_id>/', views.detail, name='detail'),
+    path('checkout/', views.checkout, name='checkout'),
+]
